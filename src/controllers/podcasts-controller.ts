2d5a71573be03054f4962f6cc0b08d386743968d
Rayaninha <salesrayane11@gmail.com>
2024-07-09 16:29:28 -0300

    feat: simple service

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,24 +1,12 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from "../services/list-episodes-service";
 
 export const listEpisodes = async (
   request: IncomingMessage,
   response: ServerResponse
 ) => {
+  const content = await serviceListEpisodes();
+
   response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(
-    JSON.stringify([
-      {
-        podcastName: "É possível imaginar o fim do capitalismo?",
-        episode: "ABC DO SOCIALISMO #01",
-        videoId: "PIdUa54WVTY",
-        categories: ["politics"],
-      },
-      {
-        podcastName: "Produção de Consenso",
-        episode: "ABC DO SOCIALISMO #02",
-        videoId: "z4d_GyRz7eA",
-        categories: ["politics"],
-      },
-    ])
-  );
+  response.end(JSON.stringify(content));
 };
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,18 @@
+export const serviceListEpisodes = async () => {
+  const data = [
+    {
+      podcastName: "É possível imaginar o fim do capitalismo?",
+      episode: "ABC DO SOCIALISMO #01",
+      videoId: "PIdUa54WVTY",
+      categories: ["politics"],
+    },
+    {
+      podcastName: "Produção de Consenso",
+      episode: "ABC DO SOCIALISMO #02",
+      videoId: "z4d_GyRz7eA",
+      categories: ["politics"],
+    },
+  ];
+
+  return data;
+};
