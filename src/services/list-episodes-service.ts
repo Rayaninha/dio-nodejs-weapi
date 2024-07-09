7bc0261d13da125287018d25df7e21e1035b60ff
Rayaninha <salesrayane11@gmail.com>
2024-07-09 16:45:14 -0300

    feat: simple repository

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,18 +1,7 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+
 export const serviceListEpisodes = async () => {
-  const data = [
-    {
-      podcastName: "É possível imaginar o fim do capitalismo?",
-      episode: "ABC DO SOCIALISMO #01",
-      videoId: "PIdUa54WVTY",
-      categories: ["politics"],
-    },
-    {
-      podcastName: "Produção de Consenso",
-      episode: "ABC DO SOCIALISMO #02",
-      videoId: "z4d_GyRz7eA",
-      categories: ["politics"],
-    },
-  ];
+  const data = await repositoryPodcast();
 
   return data;
 };
