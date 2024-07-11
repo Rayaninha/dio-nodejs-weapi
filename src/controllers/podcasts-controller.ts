ef94e0322e7202edf43c4c9263ea5c5e8d59a56c
Rayaninha <salesrayane11@gmail.com>
2024-07-11 10:26:45 -0300

    feat: routes

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,11 +1,22 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from "../services/list-episodes-service";
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
 export const listEpisodes = async (
   request: IncomingMessage,
   response: ServerResponse
 ) => {
   const content = await serviceListEpisodes();
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(content));
+};
+
+export const filterEpisodes = async (
+  request: IncomingMessage,
+  response: ServerResponse
+) => {
+  const content = await serviceFilterEpisodes("ABC DO SOCIALISMO");
 
   response.writeHead(200, { "Content-Type": "application/json" });
   response.end(JSON.stringify(content));
